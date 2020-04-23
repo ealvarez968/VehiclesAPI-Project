7592f86b250ede5ad93702785e2113337e1e373c
Eduardo Alvarez <ealvarez0896@gmail.com>
2020-04-23 08:17:58 -0600

    Vehicles-Api Done

---
+++ vehicles-api/src/main/java/com/udacity/vehicles/api/CarController.java
@@ -0,0 +1,160 @@
+package com.udacity.vehicles.api;
+
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+
+import com.udacity.vehicles.domain.car.Car;
+import com.udacity.vehicles.service.CarService;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
+import javax.validation.Valid;
+
+
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.hibernate.validator.internal.util.logging.LoggerFactory;
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.Resources;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Implements a REST-based controller for the Vehicles API.
+ */
+@RestController
+@RequestMapping("/cars")
+class CarController {
+
+    private final CarService carService;
+    private final CarResourceAssembler assembler;
+
+
+    CarController(CarService carService, CarResourceAssembler assembler) {
+        this.carService = carService;
+        this.assembler = assembler;
+    }
+
+    /**
+     * Creates a list to store any vehicles.
+     * @return list of vehicles
+     */
+    @ApiOperation(value = "List all stored vehicles")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "The Vehicles have been found."),
+            @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+            @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+            @ApiResponse(code = 404, message = "The vehicles you were trying to find doesn't exist")
+    })
+    @GetMapping
+    Resources<Resource<Car>> list() {
+        List<Resource<Car>> resources = carService.list().stream().map(assembler::toResource)
+                .collect(Collectors.toList());
+        return new Resources<>(resources,
+                linkTo(methodOn(CarController.class).list()).withSelfRel());
+    }
+
+    /**
+     * Gets information of a specific car by ID.
+     * @param id the id number of the given vehicle
+     * @return all information for the requested vehicle
+     */
+    @ApiOperation(value = "Get specific information of a car by ID")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Your vehicle has been found."),
+            @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+            @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+            @ApiResponse(code = 404, message = "Vehicle not found")
+    })
+    @GetMapping("/{id}")
+    Resource<Car> get(@PathVariable Long id) {
+        /**
+         * TODO: Use the `findById` method from the Car Service to get car information.
+         * TODO: Use the `assembler` on that car and return the resulting output.
+         *   Update the first line as part of the above implementing. (don't understand)
+         */
+        return assembler.toResource(carService.findById(id));
+    }
+
+    /**
+     * Posts information to create a new vehicle in the system.
+     * @param car A new vehicle to add to the system.
+     * @return response that the new vehicle was added to the system
+     * @throws URISyntaxException if the request contains invalid fields or syntax
+     */
+    @ApiOperation(value = "Creates a new vehicle in the system.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Vehicle has been created successfully."),
+            @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+            @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+            @ApiResponse(code = 404, message = "The vehicle you were trying to create couldn't be created because of the car parameters")
+    })
+    @PostMapping
+    ResponseEntity<?> post(@Valid @RequestBody Car car) throws URISyntaxException {
+        /**
+         * TODO: Use the `save` method from the Car Service to save the input car.
+         * TODO: Use the `assembler` on that saved car and return as part of the response.
+         *   Update the first line as part of the above implementing.
+         */
+        Resource<Car> resource = assembler.toResource(carService.save(car));
+        return ResponseEntity.created(new URI(resource.getId().expand().getHref())).body(resource);
+    }
+
+    /**
+     * Updates the information of a vehicle in the system.
+     * @param id The ID number for which to update vehicle information.
+     * @param car The updated information about the related vehicle.
+     * @return response that the vehicle was updated in the system
+     */
+    @ApiOperation(value = "Updates the information of a vehicle in the system.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Vehicle has been updated successfully."),
+            @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+            @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+            @ApiResponse(code = 404, message = "The vehicle you were trying to update wasn't found")
+    })
+    @PutMapping("/{id}")
+    ResponseEntity<?> put(@PathVariable Long id, @Valid @RequestBody Car car) {
+        /**
+         * TODO: Set the id of the input car object to the `id` input.
+         * TODO: Save the car using the `save` method from the Car service
+         * TODO: Use the `assembler` on that updated car and return as part of the response.
+         *   Update the first line as part of the above implementing.
+         */
+        car.setId(id);
+        Resource<Car> resource = assembler.toResource(carService.save(car));
+        return ResponseEntity.ok(resource);
+    }
+
+    /**
+     * Removes a vehicle from the system.
+     * @param id The ID number of the vehicle to remove.
+     * @return response that the related vehicle is no longer in the system
+     */
+    @ApiOperation(value = "Removes a vehicle from the system.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 204, message = "Deleted successfully but no content"),
+            @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+            @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+            @ApiResponse(code = 404, message = "The vehicle you were trying to delete wasn't found")
+    })
+    @DeleteMapping("/{id}")
+    ResponseEntity<?> delete(@PathVariable Long id) {
+        /**
+         * TODO: Use the Car Service to delete the requested vehicle.
+         */
+        carService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
