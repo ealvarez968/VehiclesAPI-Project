6a19ddbdb5f9a829fdb41dea71b6e36c79c12604
Eduardo Alvarez <ealvarez0896@gmail.com>
2020-04-25 17:01:23 -0600

    Eureka Server

---
+++ eurekaV2/src/main/java/com/udacity/eurekaV2/EurekaV2Application.java
@@ -0,0 +1,13 @@
+package com.udacity.eurekaV2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaV2Application.class, args);
+	}
+
+}
